25632f8f60d957aa43cfe30e1ac62f1012b2c150
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-31 04:00:18 -0300

    feat: add QueryClientProvider

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,11 +4,17 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './components/Router'
 import { UserContextProvider } from './contexts/UserContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <RouterProvider router={router} />
-    </UserContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <RouterProvider router={router} />
+      </UserContextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
