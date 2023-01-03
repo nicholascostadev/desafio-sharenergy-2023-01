8ff90f11891c6ac29ad9133a1ea96ccf2a2ac78d
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-03 00:13:33 -0300

    feat: use cors, cookieParser and expressJson()

--- back-end/src/bootstrap.ts
+++ back-end/src/bootstrap.ts
@@ -1,8 +1,14 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import { authRoutes } from './routes/authRoutes'
+import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const PORT = 4444
 const app = express()
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
