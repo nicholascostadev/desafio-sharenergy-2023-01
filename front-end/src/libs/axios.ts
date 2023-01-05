abe7b8a7f98a7dfab340ab50e1f59d8626214bda
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-05 19:28:51 -0300

    fix: add production database URL

--- front-end/src/libs/axios.ts
+++ front-end/src/libs/axios.ts
@@ -2,9 +2,13 @@ import axios from 'axios'
 
 export const api = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:4444',
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_DATABASE_URL
+    : 'http://localhost:4444',
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:4444',
+    'Access-Control-Allow-Origin': import.meta.env.PROD
+      ? import.meta.env.VITE_DATABASE_URL
+      : 'http://localhost:4444',
     'Access-Control-Allow-Credentials': 'true',
   },
 })
