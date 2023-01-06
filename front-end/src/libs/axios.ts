c354a5dbac550754f52c1ce64a38de89bfb8f0c4
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-05 21:34:47 -0300

    try to fix cors again

--- front-end/src/libs/axios.ts
+++ front-end/src/libs/axios.ts
@@ -6,9 +6,6 @@ export const api = axios.create({
     ? import.meta.env.VITE_DATABASE_URL
     : 'http://localhost:4444',
   headers: {
-    'Access-Control-Allow-Origin': import.meta.env.PROD
-      ? import.meta.env.VITE_DATABASE_URL
-      : 'http://localhost:4444',
     'Access-Control-Allow-Credentials': 'true',
   },
 })
