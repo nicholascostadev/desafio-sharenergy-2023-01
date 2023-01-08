ba0c65dd3083eec638ca533ad2f7f583a6f50db6
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-08 11:17:14 -0300

    feat: add interceptors utils

---
+++ back-end/src/__tests__/utils/interceptors.ts
@@ -0,0 +1,16 @@
+export const mockRequest = () => {
+  const req = {} as any
+  req.body = jest.fn().mockReturnValue(req)
+  req.params = jest.fn().mockReturnValue(req)
+  return req
+}
+
+export const mockResponse = () => {
+  const res = {} as any
+  res.send = jest.fn().mockReturnValue(res)
+  res.status = jest.fn().mockReturnValue(res)
+  res.json = jest.fn().mockReturnValue(res)
+  return res
+}
+
+export const mockNext = () => jest.fn()
