2dff71a123db5a1ac7b9d43bf37e3f7cc8eb868f
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-09 11:04:08 -0300

    refactor + fix: remove unreachable code + fix edge case

--- back-end/src/utils/parseAuthorization.ts
+++ back-end/src/utils/parseAuthorization.ts
@@ -1,13 +1,14 @@
 export const parseAuthorization = (token: string | undefined): string | null => {
   if (token == null) return null
   const authorization = token
-  if (authorization == null) {
-    return null
-  }
 
-  const split = authorization.split('Bearer')
+  const split = authorization.trim().split('Bearer')
 
   if (split.length !== 2) return null
+
+  const isEmpty = split[1].trim() === ''
+
+  if (isEmpty) return null
 
   return authorization.split('Bearer')[1].trim()
 }
