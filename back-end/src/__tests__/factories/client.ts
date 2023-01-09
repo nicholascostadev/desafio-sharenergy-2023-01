eb826236984b01c5b458a385c145fc7a915709fd
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-09 14:01:18 -0300

    feat: add faker to clientFactory

--- back-end/src/__tests__/factories/client.ts
+++ back-end/src/__tests__/factories/client.ts
@@ -1,15 +1,21 @@
-export const clientFactory = () => ({
-  name: 'John Doe',
-  email: 'johndoe@gmail.com',
-  telephone: '12345678910',
-  cpf: '51589662067',
+import { faker } from '@faker-js/faker'
+import { generate } from 'gerador-validador-cpf'
+interface ClientFactoryProps {
+  name?: string
+}
+
+export const clientFactory = (props?: ClientFactoryProps) => ({
+  name: props?.name ?? faker.name.fullName(),
+  email: faker.internet.email(),
+  telephone: faker.phone.number('###########'),
+  cpf: generate(),
   address: {
-    street: 'street 1',
-    number: '1',
-    additionalInfo: 'complement 1',
-    neighborhood: 'neighborhood 1',
-    city: 'city 1',
-    state: 'state 1',
-    zipCode: '12312312'
+    street: faker.address.street(),
+    number: faker.address.buildingNumber(),
+    additionalInfo: 'fake additional info',
+    neighborhood: 'fake neighborhood',
+    city: faker.address.cityName(),
+    state: faker.address.state(),
+    zipCode: faker.address.zipCode('########')
   }
 })
